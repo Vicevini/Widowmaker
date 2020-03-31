073763b6fa8d537a232dea19ed3ba504812bd9f4
Vinicius Oliveira <vicevini@live.com>
2020-03-31 20:18:31 -0300

    Mudando método da API

--- src/app/dados.service.ts
+++ src/app/dados.service.ts
@@ -6,12 +6,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DadosService {
 
-  // dadosURL = 'https://4d084eed-6895-49c1-b8a7-dc28c5f032a9.mock.pstmn.io/usuarios';
-   dadosURL = 'http://localhost:3000/usuario';
+    dadosURL = 'http://4d084eed-6895-49c1-b8a7-dc28c5f032a9.mock.pstmn.io/usuarios';
+   //dadosURL = 'http://localhost:3000/usuario';
 
   constructor(private http: HttpClient) { }
 
   listar(){
-    return this.http.get<any[]>(`${this.dadosURL}`);
+
+    //return this.http.post<any[]>(`${this.dadosURL}`);
+    //return this.http.get<any[]>(`${this.dadosURL}`);
+
+    return this.http.post(this.dadosURL).pipe(take(1));
   }
 }
